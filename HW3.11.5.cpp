6aa19d9f8f9322ab8e4f7b23f659f120632b539f
Mikhail Dubrovskiy <dmaz68@mail.ru>
2023-05-03 20:31:26 +0300

    Initial commit

---
+++ HW3.11.5.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+#include "Figure.h"
+#include "Triangle.h"
+#include "Quadrilateral.h"
+#include "PrintFigures.h"
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	system("chcp 1251");
+
+	FigureLibraryDynamic::Triangle Triangle1(10, 20, 30, 40, 50, 60);
+	FigureLibraryDynamic::printFigures(&Triangle1);
+
+	FigureLibraryDynamic::RightTriangle rightTriangle1(10, 20, 30, 40, 50);
+	FigureLibraryDynamic::printFigures(&rightTriangle1);
+
+	FigureLibraryDynamic::IsoscelesTriangle isoscelesTriangle1(11, 220, 330, 440);
+	FigureLibraryDynamic::printFigures(&isoscelesTriangle1);
+
+	FigureLibraryDynamic::EquilateralTriangle equilateralTriangle1(111);
+	FigureLibraryDynamic::printFigures(&equilateralTriangle1);
+
+	FigureLibraryDynamic::Quadrilateral Quadrilateral1(10, 20, 30, 40, 50, 60, 70, 80);
+	FigureLibraryDynamic::printFigures(&Quadrilateral1);
+
+	FigureLibraryDynamic::Rectangle Rectangle1(14, 24);
+	FigureLibraryDynamic::printFigures(&Rectangle1);
+
+	FigureLibraryDynamic::Square Square1(333);
+	FigureLibraryDynamic::printFigures(&Square1);
+
+	FigureLibraryDynamic::Parallelogram Parallelogram1(555, 666, 777, 888);
+	FigureLibraryDynamic::printFigures(&Parallelogram1);
+
+	FigureLibraryDynamic::Rhomb Rhomb1(14, 24, 34);
+	FigureLibraryDynamic::printFigures(&Rhomb1);
+}
---
+++ Triangle.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "Figure.h"
+
+#ifdef FigureLibraryDynamic_EXPORTS
+#define FigureLibraryDynamic_API __declspec(dllexport)
+#else
+#define FigureLibraryDynamic_API __declspec(dllimport)
+#endif
+
+namespace FigureLibraryDynamic
+{
+	class FigureLibraryDynamic_API Triangle : public Figure
+	{
+	public:
+		Triangle(int sideLengthA, int sideLengthB, int sideLengthC,
+			int angleA, int angleB, int angleC);
+	};
+
+	class FigureLibraryDynamic_API RightTriangle : public Triangle
+	{
+	public:
+		RightTriangle(int sideLengthA, int sideLengthB, int sideLengthC, int angleA, int angleB);
+	};
+
+	class FigureLibraryDynamic_API IsoscelesTriangle : public Triangle
+	{
+	public:
+		IsoscelesTriangle(int sideLengthA, int sideLengthB, int angleA, int angleB);
+	};
+
+	class FigureLibraryDynamic_API EquilateralTriangle :
+		public Triangle
+	{
+	public:
+		EquilateralTriangle(int sideLengthA);
+	};
+};
